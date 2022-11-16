e8da2f35d8467ce9bd972d4f1a0a969aed8863de
rqthomas <rqthomas@vt.edu>
2022-11-16 14:03:44 +0000

    notes on cron timing

--- cron_timing.R
+++ cron_timing.R
@@ -7,17 +7,6 @@ home_dir <-  path.expand("~/aws_noaa")
 log_dir <- path.expand("~/log/cron")
 fs::dir_create(log_dir)
 
-## GEFS arrow
-#the 384 Horizon is finished at 6:30 AM UTC on same day
-#Note the Cron timing on the machine is UTC
-cmd <- cronR::cron_rscript(rscript = file.path(home_dir, "combined_stages.R"),
-                           rscript_log = file.path(log_dir, "noaa_gefs_today.log"),
-                           log_append = FALSE,
-                           cmd = "/usr/local/bin/r", # use litter, more robust on CLI
-                           workdir = file.path(home_dir),
-                           trailing_arg = "curl -fsS -m 10 --retry 5 -o /dev/null https://hc-ping.com/6c3dec04-631a-4a8e-8c55-8837f2827e07")
-cronR::cron_add(command = cmd, frequency = '0 7 * * *', id = 'noaa_gefs_today')
-
 #the 840 Horizon is finished at 3:05 AM UTC on next day
 #Note the Cron timing on the machine is UTC
 cmd <- cronR::cron_rscript(rscript = file.path(home_dir, "stage1_yesterday.R"),
@@ -27,4 +16,18 @@ cmd <- cronR::cron_rscript(rscript = file.path(home_dir, "stage1_yesterday.R"),
                            workdir = file.path(home_dir),
                            trailing_arg = "curl -fsS -m 10 --retry 5 -o /dev/null https://hc-ping.com/3c7408aa-070a-4985-a856-356606a297b7")
 cronR::cron_add(command = cmd, frequency = '0 4 * * *', id = 'noaa_gefs_yesterday')
+
+#3 hours is enough time for yesterday to finish downloading before downloading today
+
+## GEFS arrow
+#the 384 Horizon is finished at 6:30 AM UTC on same day
+#Note the Cron timing on the machine is UTC
+cmd <- cronR::cron_rscript(rscript = file.path(home_dir, "combined_stages_today.R"),
+                           rscript_log = file.path(log_dir, "noaa_gefs_today.log"),
+                           log_append = FALSE,
+                           cmd = "/usr/local/bin/r", # use litter, more robust on CLI
+                           workdir = file.path(home_dir),
+                           trailing_arg = "curl -fsS -m 10 --retry 5 -o /dev/null https://hc-ping.com/6c3dec04-631a-4a8e-8c55-8837f2827e07")
+cronR::cron_add(command = cmd, frequency = '0 7 * * *', id = 'noaa_gefs_today')
+
 
