e787429383d301e9b0ed42737896c5b1f6faf03d
rqthomas <rqthomas@vt.edu>
2024-02-06 00:16:30 +0000

    updating backup

--- backup_s3_to_osn.R
+++ backup_s3_to_osn.R
@@ -8,6 +8,9 @@ minioclient::mc_alias_set("osn",
                           Sys.getenv("OSN_KEY"),
                           Sys.getenv("OSN_SECRET"))
 
-minioclient::mc_mirror(from = "vt_s3", to = "osn/bio230121-bucket01/vt_backup")
+minioclient::mc_mirror(from = "vt_s3/drivers/noaa/gefs-v12-reprocess", to = "osn/bio230121-bucket01/vt_backup/drivers/noaa/gefs-v12-reprocess")
+minioclient::mc_mirror(from = "vt_s3/forecasts", to = "osn/bio230121-bucket01/vt_backup/forecasts")
+minioclient::mc_mirror(from = "vt_s3/scores/parquet", to = "osn/bio230121-bucket01/vt_backup/scores/parquet")
+minioclient::mc_mirror(from = "vt_s3/restart", to = "osn/bio230121-bucket01/vt_backup/restart")
 
 RCurl::url.exists("https://hc-ping.com/07b470d1-3c51-4d05-808b-a89d3888c579", timeout = 5)
