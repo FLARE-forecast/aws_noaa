128cb81ce68d974422f24cdd99e712dbfa848e23
Quinn Thomas <rqthomas@vt.edu>
2024-02-28 12:29:46 -0500

    Update download_psuedo.R

--- download_psuedo.R
+++ download_psuedo.R
@@ -4,14 +4,13 @@ library(gefs4cast)
 
 gdalcubes::gdalcubes_options(parallel=34)
 #gdalcubes::gdalcubes_options(parallel=TRUE)
-readRenviron("/home/rstudio/.Renviron")
+#readRenviron("/home/rstudio/.Renviron")
 
 sites <- readr::read_csv("site_list_v2.csv")
 
 Sys.setenv("GEFS_VERSION"="v12")
-dates <- seq(as.Date("2020-09-24"), Sys.Date()-1, by=1)
 dates_pseudo <- seq(as.Date("2020-09-26"), Sys.Date(), by=1)
-dates_pseudo <- seq(as.Date("2020-10-01"), as.Date("2020-10-23"), by=1)
+#dates_pseudo <- seq(as.Date("2020-10-01"), as.Date("2020-10-23"), by=1)
 message("GEFS v12 pseudo")
 
 s3 <- gefs_s3_dir("pseudo", path = "flare/drivers/met", endpoint = "https://renc.osn.xsede.org", bucket = "bio230121-bucket01")
