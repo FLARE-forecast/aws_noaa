a48cf16ccbc79897573f9c6eec400339c6a011e8
Quinn Thomas <rqthomas@vt.edu>
2024-02-28 12:48:27 -0500

    Update download_psuedo.R

--- download_psuedo.R
+++ download_psuedo.R
@@ -9,7 +9,7 @@ gdalcubes::gdalcubes_options(parallel=34)
 sites <- readr::read_csv("site_list_v2.csv")
 
 Sys.setenv("GEFS_VERSION"="v12")
-dates_pseudo <- seq(as.Date("2020-09-26"), Sys.Date(), by=1)
+dates_pseudo <- seq(as.Date("2020-10-01"), Sys.Date(), by=1)
 #dates_pseudo <- seq(as.Date("2020-10-01"), as.Date("2020-10-23"), by=1)
 message("GEFS v12 pseudo")
 
