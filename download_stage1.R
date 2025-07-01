61ea173b0edfaaeb21a464eecd5ddf9280328b51
Austin Delany <addelany@gmail.com>
2025-07-01 13:33:59 -0500

    Update download_stage1.R

--- download_stage1.R
+++ download_stage1.R
@@ -15,8 +15,7 @@ dates <- seq(as.Date("2024-02-01"), Sys.Date()-1, by=1)
 message("GEFS v12 stage1")
 s3 <- gefs_s3_dir("stage1", path = "flare/drivers/met", endpoint = "https://amnh1.osn.mghpcc.org", bucket = "bio230121-bucket01")
 
-#have_dates <- gsub("reference_datetime=", "", s3$ls())
-#missing_dates <- dates[!(as.character(dates) %in% have_dates)]
+have_dates <- gsub("reference_datetime=", "", s3$ls())
+missing_dates <- dates[!(as.character(dates) %in% have_dates)]
 
-missing_dates <- as.Date("2025-06-27")
 gefs_to_parquet(missing_dates, path = s3, sites = sites, cycle = "00")
