ac7add48ec3607b519e476403af04ae61952ab70
Austin Delany <addelany@gmail.com>
2025-07-31 14:42:23 -0500

    Update update_stage3.R

--- update_stage3.R
+++ update_stage3.R
@@ -74,7 +74,10 @@ message('starting download loop...')
       dplyr::bind_rows(stage3_df_update) |>
       dplyr::arrange(variable, datetime, parameter) #|>
       #arrow::write_dataset(path = s3, partitioning = "site_id")
-    
+
+    print(names(df_final)
+    print(nrow(df_final))
+          
     message('save stage3...')
     duckdbfs::write_dataset(df_final, path = "s3://bio230121-bucket01/flare/drivers/met/gefs-v12/stage3", format = 'parquet',
                               partitioning = "site_id")
