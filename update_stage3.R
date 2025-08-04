56c09d5cae704052c8baf54003cdab01fddb9dc9
Austin Delany <addelany@gmail.com>
2025-08-04 08:02:43 -0500

    Update update_stage3.R

--- update_stage3.R
+++ update_stage3.R
@@ -18,7 +18,7 @@ message('starting loop...')
 #furrr::future_walk(site_list, function(curr_site_id){
 
 #purrr::walk(site_list, function(curr_site_id){  
-site_list <- c("BARC", "CRAM")
+site_list <- c("BARC")
   
 
 for (site in site_list){
