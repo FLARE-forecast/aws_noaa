77d7e1c1b38168072c8beacce7752fefa510613c
addelany <addelany@gmail.com>
2025-08-06 14:22:24 -0500

    remove duckdbfs secrets

--- download_psuedo.R
+++ download_psuedo.R
@@ -15,10 +15,10 @@ dates_pseudo <- seq(as.Date("2020-10-01"), Sys.Date(), by=1)
 #dates_pseudo <- seq(as.Date("2020-10-01"), as.Date("2020-10-23"), by=1)
 message("GEFS v12 pseudo")
 
-duckdbfs::duckdb_secrets(
-  endpoint = 'amnh1.osn.mghpcc.org',
-  key = Sys.getenv("OSN_KEY"),
-  secret = Sys.getenv("OSN_SECRET"))
+# duckdbfs::duckdb_secrets(
+#   endpoint = 'amnh1.osn.mghpcc.org',
+#   key = Sys.getenv("OSN_KEY"),
+#   secret = Sys.getenv("OSN_SECRET"))
 
 s3 <- gefs_s3_dir("pseudo", path = "flare/drivers/met", endpoint = "https://amnh1.osn.mghpcc.org", bucket = "bio230121-bucket01")
 have_dates <- gsub("reference_datetime=", "", s3$ls())
