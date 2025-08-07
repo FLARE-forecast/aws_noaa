639c1c54d9670cefa925db6f3429ecdfdae3113f
Austin Delany <addelany@gmail.com>
2025-08-07 13:23:24 -0500

    Update download_stage1.R

--- download_stage1.R
+++ download_stage1.R
@@ -1,6 +1,7 @@
 library(gdalcubes)
 library(gefs4cast)
-install.packages('arrow',version = '20.0.0')
+#install.packages('arrow',version = '20.0.0')
+remotes::install_version("arrow", version = "20.0.0")
 library(arrow)
 
 print(sessioninfo::package_info())
